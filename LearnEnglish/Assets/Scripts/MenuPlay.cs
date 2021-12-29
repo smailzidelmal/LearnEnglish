6d4f8ec21c9fa530f6379f998e3855f919efe750
smailzidelmal <smilzdl@hotmail.com>
2021-12-29 17:47:49 +0100

    Menu play exit  , EndMenu exit Return to Menu

--- LearnEnglish/Assets/Scripts/MenuPlay.cs
+++ LearnEnglish/Assets/Scripts/MenuPlay.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class MenuPlay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public  GameObject Menu;  
+    
+    void OnMouseDown(){
+   	this.Menu.SetActive(false);	
+   }
 }
