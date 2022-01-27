11de57dbc47f46b2d2424825a511a0589494ec8d
axelfoltyn <axel.foltyn@gmail.com>
2022-01-27 13:30:19 +0100

    start read file player and remove cardio.txt

--- LearnEnglish/Assets/Scripts/GeneralInfo.cs
+++ LearnEnglish/Assets/Scripts/GeneralInfo.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public static class GeneralInfo
 {
@@ -28,4 +30,31 @@ public static class GeneralInfo
     public static bool AnswerCorrect(){
         return correct;
     }
+
+    public static void parsefileplayer(string file){
+        string path_file = Path.Combine(Application.dataPath ,Path.GetFullPath(file));
+        Dictionary<string, List<string>> res_dict = new Dictionary<string, List<string>>();
+        using(var reader = new StreamReader(path_file))
+        {
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var line_split = line.Split(';');
+                try {
+                    res_dict.Add(line_split[0], new List<string>());
+                    for (int i = 1; i < line_split.Length; i++){
+                        res_dict[line_split[0]].Add(line_split[i]);
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    Debug.Log("" + line_split[0] + "exist two time");
+                }
+
+            }
+        }
+        Debug.Log("=>cd"+res_dict);
+    }
+
 }
