3e05c5ed0dc3916f3484cf3c80ba75a07876879c
axelfoltyn <axel.foltyn@gmail.com>
2022-01-29 12:36:16 +0100

    rm old cardio.py and finish parser of csv file

--- LearnEnglish/Assets/Scripts/GeneralInfo.cs
+++ LearnEnglish/Assets/Scripts/GeneralInfo.cs
@@ -37,24 +37,40 @@ public static class GeneralInfo
         using(var reader = new StreamReader(path_file))
         {
             reader.ReadLine();
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                var line_split = line.Split(';');
-                try {
-                    res_dict.Add(line_split[0], new List<string>());
-                    for (int i = 1; i < line_split.Length; i++){
-                        res_dict[line_split[0]].Add(line_split[i]);
+            var line = reader.ReadLine();
+            var line_split = line.Split(';');
+            try {
+                //recupération du login est des donées général (cardio moyen et son niveau)
+                res_dict.Add("gene", new List<string>());
+                for (int i = 1; i < line_split.Length; i++){
+                    res_dict["gene"].Add(line_split[i]);
+                }
+                // recupération des réponse au questions
+                res_dict.Add("question", new List<string>());
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    line_split = line.Split(';');
+                    if (int.TryParse(line_split[0], out _)){
+                        res_dict["question"].Add(line_split[1]);
+                    }
+                    else{
+                        Debug.Log("" + line_split[0] + "not number"); 
+                        break;      
                     }
                 }
-                catch (ArgumentException)
-                {
-                    Debug.Log("" + line_split[0] + "exist two time");
-                }
-
+            }
+            catch (ArgumentException)
+            {
+                Debug.Log("" + line_split[0] + "exist two time");
             }
         }
-        Debug.Log("=>cd"+res_dict);
+        Debug.Log("dico=>"+res_dict);
+        foreach (KeyValuePair<string, List<string>> kvp in res_dict)
+        {
+            Debug.Log("  Key = "+ kvp.Key +" value = " + kvp.Value);
+            kvp.Value.ForEach(Debug.Log);
+        }
     }
 
 }
